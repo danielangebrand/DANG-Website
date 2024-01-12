feffd75be9a77496e4ef61ed501034d0e77caf02
Daniel <danielangebrand@outlook.com>
2024-01-12 19:43:44 +0100

    Add project files.

---
+++ Components/Bulletin/Bulletine.cs
@@ -0,0 +1,9 @@
+﻿namespace Homepage.Components.BulletinComponent
+{
+    public class Bulletine
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Relationship { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
---
+++ Components/Projects/Project.cs
@@ -0,0 +1,14 @@
+﻿namespace Homepage.Components.Projects
+{
+    public class Project
+    {
+        public string Image { get; set; } = string.Empty;
+
+        public string Subtitle { get; set; } = string.Empty;
+
+        public string Title { get; set; } = string.Empty;
+
+        public string GithubLink { get; set; } = string.Empty;
+        public string? ActiveLink { get; set; }
+    }
+}
