60cf869b022d84cffdbef37b8ab1cbe325cd4c13
Daniel <danielangebrand@outlook.com>
2024-01-15 11:11:58 +0100

    Latest updates

--- wwwroot/Assets/JS/main.js
+++ wwwroot/Assets/JS/main.js
@@ -179,17 +179,18 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+window.onload = function () { 
+    const sr = ScrollReveal({
+        origin: 'top',
+        distance: '60px',
+        duration: 2500,
+        delay: 400,
+        reset: true
+    });
 
-const sr = ScrollReveal({
-  origin: 'top',
-  distance: '60px',
-  duration: 2500,
-  delay: 400,
-  reset: true
-})
 sr.reveal(`.home__data, .projects__container, .bulletin__container, .footer__container`)
-sr.reveal(`.home__info div`), {delay: 600, origin: 'bottom', interval: 100}
-sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`), {origin: 'left'}
-sr.reveal(`.skills__content:nth-child(2), .contact__content:nth-child(2)`), {origin: 'right'}
-sr.reveal(`.skills__content:nth-child(3), .contact__content:nth-child(3)`), {origin: 'under'}
-sr.reveal(`.qualification__content`, {interval: 100})
+sr.reveal(`.home__info div`, {delay: 600, origin: 'bottom', interval: 100})
+sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, {origin: 'left'})
+sr.reveal(`.skills__content:nth-child(2), .contact__content:nth-child(2)`, {origin: 'right'})
+sr.reveal(`.skills__content:nth-child(3), .contact__content:nth-child(3)`, {origin: 'under'})
+sr.reveal(`.qualification__content`, { interval: 100 })}
